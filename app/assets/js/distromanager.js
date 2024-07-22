afd41960915657a334ccc95f7eaad183a23097fd
NhProGamer <neo.huyghe@gmail.com>
2024-07-22 15:41:17 +0200

    Tiny fixes

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://mc.nhsoul.fr/distro.json'
+exports.REMOTE_DISTRO_URL = 'https://mc.nhsoul.fr/launcher/distro.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
